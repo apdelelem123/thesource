1dfd4464ac90662b0323aafde8c711885881c888
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-02-17 14:44:28 +0300

    Update ايدي.py

--- Tepthon/plugins/ايدي.py
+++ Tepthon/plugins/ايدي.py
@@ -39,10 +39,10 @@ LOGS = logging.getLogger(__name__)
 # code by t.me/zzzzl1l
 ZED_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "•⎚• مـعلومـات المسـتخـدم مـن بـوت ريبثون"
 ZEDM = gvarstatus("CUSTOM_ALIVE_EMOJI") or "✦ "
-ZEDF = gvarstatus("CUSTOM_ALIVE_FONT") or "⋆─┄─┄─┄─ سورس الزعيم ─┄─┄─┄─⋆"
-zed_dev = (6700336976, 6060337233, 6373798952)
-zel_dev = (6700336976, 6060337233, 6373798952)
-zelzal = (6700336976, 1264384082)
+ZEDF = gvarstatus("CUSTOM_ALIVE_FONT") or "⋆─┄─┄─┄─surs alzaeim─┄─┄─┄─⋆"
+zed_dev = (6700336976, 1991212284, 6373798952)
+zel_dev = (6700336976, 1991212284, 6373798952)
+zelzal = (6700336976, 1991212284)
 
 
 async def get_user_from_event(event):
